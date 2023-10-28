eb858862f0dcce07f4f139c2dc6a17db94496b77
MuktharAmiyan <amiyanmukthar@gmail.com>
2023-10-28 17:23:14 +0530

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Mukthar Amiyan on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
